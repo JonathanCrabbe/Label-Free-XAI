225fcf4716b26f0f5284cfa8d7c934cffcac0971
JonathanCrabbe <jonathan.cr1302@gmail.com>
2021-11-26 15:02:09 +0000

    Add plots for pixel perturbation experiment

--- experiments/mnist.py
+++ experiments/mnist.py
@@ -1,6 +1,9 @@
 import torch
+import os
 import torchvision
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 from torchvision import transforms
 from torch.utils.data import DataLoader, random_split
 from models.images import EncoderMnist, DecoderMnist, train_epoch, test_epoch
@@ -9,7 +12,7 @@ from captum.attr import GradientShap, DeepLift, IntegratedGradients, Saliency
 from explanations.features import AuxiliaryFunction
 
 
-def denoiser_mnist(random_seed: int = 0, batch_size: int = 200, dim_latent: int = 4, n_epochs: int = 5):
+def denoiser_mnist(random_seed: int = 1, batch_size: int = 200, dim_latent: int = 4, n_epochs: int = 30):
     # Initialize seed and device
     torch.random.manual_seed(random_seed)
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -53,7 +56,6 @@ def denoiser_mnist(random_seed: int = 0, batch_size: int = 200, dim_latent: int 
 
         print(f'\n Epoch {epoch + 1}/{n_epochs} \t train loss {train_loss:.3g} \t val loss {val_loss:.3g} \t ')
 
-
     attr_dic = {'Gradient Shap': np.zeros((len(test_dataset), 1, 28, 28)),
                 'Integrated Gradients': np.zeros((len(test_dataset), 1, 28, 28)),
                 'DeepLift': np.zeros((len(test_dataset), 1, 28, 28)),
@@ -62,7 +64,8 @@ def denoiser_mnist(random_seed: int = 0, batch_size: int = 200, dim_latent: int 
     rep_shift_dic = {'Gradient Shap': np.zeros((len(n_pixels_pert), len(test_loader))),
                      'Integrated Gradients': np.zeros((len(n_pixels_pert), len(test_loader))),
                      'DeepLift': np.zeros((len(n_pixels_pert), len(test_loader))),
-                     'Saliency': np.zeros((len(n_pixels_pert), len(test_loader)))}
+                     'Saliency': np.zeros((len(n_pixels_pert), len(test_loader))),
+                     'Random': np.zeros((len(n_pixels_pert), len(test_loader)))}
 
     for n_batch, (batch_images, _) in enumerate(test_loader):
         batch_images = batch_images.to(device)
@@ -72,24 +75,44 @@ def denoiser_mnist(random_seed: int = 0, batch_size: int = 200, dim_latent: int 
                          'DeepLift': DeepLift(auxiliary_encoder),
                          'Saliency': Saliency(auxiliary_encoder)
                          }
-        for explainer in explainer_dic:
+        for explainer in rep_shift_dic:
             if explainer in ["Gradient Shap", "Integrated Gradients", "DeepLift"]:
                 attr_batch = explainer_dic[explainer].attribute(batch_images, baseline_features)
-            else:
+            elif explainer in ["Saliency"]:
                 attr_batch = explainer_dic[explainer].attribute(batch_images)
             for pert_id, n_pixels in enumerate(n_pixels_pert):
-                top_pixels = torch.topk(torch.abs(attr_batch).view(len(batch_images), -1), n_pixels)[1]
                 mask = torch.ones(batch_images.shape, device=device)
-                for k in range(n_pixels):
-                    mask[:, 0, top_pixels[:, k] // 28, top_pixels[:, k] % 28] = 0
+                if explainer in explainer_dic.keys():
+                    top_pixels = torch.topk(torch.abs(attr_batch).view(len(batch_images), -1), n_pixels)[1]
+                    for k in range(n_pixels):
+                        mask[:, 0, top_pixels[:, k] // 28, top_pixels[:, k] % 28] = 0
+                    attr_dic[explainer][n_batch * batch_size:(n_batch * batch_size + len(batch_images))] = \
+                        attr_batch.detach().cpu().numpy()
+                elif explainer == "Random":
+                    for batch_id in range(len(batch_images)):
+                        top_pixels = torch.randperm(28**2)[:n_pixels]
+                        for k in range(n_pixels):
+                            mask[batch_id, 0, top_pixels[k] // 28, top_pixels[k] % 28] = 0
                 batch_images_pert = mask*batch_images
                 representation_shift = torch.sqrt(torch.sum((encoder(batch_images_pert)-encoder(batch_images))**2, -1))
                 rep_shift_dic[explainer][pert_id, n_batch] = torch.mean(representation_shift).cpu().detach().numpy()
-            attr_dic[explainer][n_batch * batch_size:(n_batch * batch_size + len(batch_images))] = attr_batch.detach().cpu().numpy()
-    print(rep_shift_dic["Integrated Gradients"].mean(axis=-1))
-    print(rep_shift_dic["Gradient Shap"].mean(axis=-1))
-    print(rep_shift_dic["DeepLift"].mean(axis=-1))
-    print(rep_shift_dic["Saliency"].mean(axis=-1))
+
+    sns.set_style("white")
+    sns.set_palette("colorblind")
+    for explainer in rep_shift_dic:
+        plt.plot(n_pixels_pert, rep_shift_dic[explainer].mean(axis=-1), label=explainer)
+        plt.fill_between(n_pixels_pert, rep_shift_dic[explainer].mean(axis=-1) - rep_shift_dic[explainer].std(axis=-1),
+                         rep_shift_dic[explainer].mean(axis=-1) + rep_shift_dic[explainer].std(axis=-1), alpha=0.4)
+    plt.xlabel("Number of Pixels Perturbed")
+    plt.ylabel("Latent Shift")
+    plt.legend()
+    results_dir = Path.cwd() / "experiments/results/mnist"
+    if not results_dir.exists():
+        os.makedirs(results_dir)
+    plt.savefig(results_dir/"pixel_pert.pdf")
+    plt.show()
+
+
 
 
     '''
